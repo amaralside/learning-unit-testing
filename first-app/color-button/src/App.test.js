812e22a9dd9c8dbdb85420e9fe1a58bf7e52fe07
amaralside <amaraldarien@gmail.com>
2022-11-23 18:06:33 -0300

    unit testing functions

--- first-app/color-button/src/App.test.js
+++ first-app/color-button/src/App.test.js
@@ -1,5 +1,6 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
+import { replaceCamelWithSpaces } from './App';
 
 test('button has correct initial color, and updates when clicked', () => {
   render(<App />);
@@ -78,3 +79,17 @@ test('clicked disabled button has gray background and reverts to blue', () => {
   fireEvent.click(checkbox);
   expect(colorButton).toHaveStyle({ backgroundColor: 'blue' });
 });
+
+describe('spaces before camel-case capital letters', () => {
+  test('works for no inner capital letter', () => {
+    expect(replaceCamelWithSpaces('Red')).toBe('Red');
+  });
+
+  test('works for one inner capital letter', () => {
+    expect(replaceCamelWithSpaces('MidnightBlue')).toBe('Midnight Blue');
+  });
+
+  test('works for multiple inner capital letters', () => {
+    expect(replaceCamelWithSpaces('MidnightVioletRed')).toBe('Midnight Violet Red');
+  });
+})
