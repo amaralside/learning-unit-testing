2d93cf43d49644a0ab6b85e02c6391c0418d58af
Darien Amaral Martinez <amaraldarien@gmail.com>
2023-01-25 12:57:14 -0300

    this is finished

--- second-app/sundaes-on-demand/src/App.js
+++ second-app/sundaes-on-demand/src/App.js
@@ -1,16 +1,32 @@
+import { useState } from 'react';
 import Container from 'react-bootstrap/Container';
+import OrderConfirmation from './pages/confirmation/OrderConfirmation';
 import OrderEntry from './pages/entry/OrderEntry';
+import OrderSummary from './pages/summary/OrderSummary';
 import { OrderDetailsProvider } from './contexts/OrderDetails';
 
 function App() {
+  // orderPhase need to be 'inProgress', 'review' or 'completed'
+  const [orderPhase, setOrderPhase] = useState('inProgress');
+
+  let Component = OrderEntry; // default to order page
+  switch (orderPhase) {
+    case 'inProgress':
+      Component = OrderEntry;
+      break;
+    case 'review':
+      Component = OrderSummary;
+      break;
+    case 'completed':
+      Component = OrderConfirmation;
+      break;
+    default:
+  }
+
   return (
-    <Container>
-      <OrderDetailsProvider>
-        {/* summary page and entry page need provider */}
-        <OrderEntry />
-      </OrderDetailsProvider>
-      {/* confirmation page does not need provider */}
-    </Container>
+    <OrderConfirmation>
+      <Container>{<Component setOrderPhase={setOrderPhase} />}</Container>
+    </OrderConfirmation>
   );
 }
 
